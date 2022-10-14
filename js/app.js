d8d789a09c174c7c5ada8699a90f145ec6a2ca3b
guillsil <111441275+guillsil@users.noreply.github.com>
2022-10-13 21:51:38 -0300

    update

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ var dic = {
     "codoacodo@gmail.com": "moveflix",
     "admin@gmail.com": "moveflix"
 };
-
+/* scrip para verificar el login */
 function validate(){
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
@@ -14,3 +14,13 @@ function validate(){
         alert("Usuario o contraseña incorrectos");
     }
 }
+/* boton de alerta de pagina no disponible */
+function mostrar(){
+    swal({
+        title: "Lo sentimos",
+        text: "Este contenido no esta disponible en tu pais",
+        icon: "warning",
+        button: "Aceptar",
+    });
+}
+/* funciones para validar el foemulario */
--- js/main.js
+++ js/main.js
@@ -57,3 +57,6 @@ closeBtn.onclick = ()  => {
 };
 
 
+
+
+
